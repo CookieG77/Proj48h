05709601529798e2de19235fdc4bbcbd81e09244
Maxime Cordonnier <mcordonn9277@gmail.com>
2025-03-11 15:38:18 +0100

    ptit fix

--- Projet/backend/pages/HomePageHandler.go
+++ Projet/backend/pages/HomePageHandler.go
@@ -8,9 +8,8 @@ import (
 // HomePage is the handler for the home page.
 func HomePage(w http.ResponseWriter, r *http.Request) {
 	PageInfo := f.NewContentInterface("home", w, r)
-	tmpl := f.MakeTemplate(w, "templates/home.html")
 	fileID := f.GenerateHexFilename()
 	f.SetCookie(w, "FileID", fileID)
 	f.AddAdditionalScriptsToContentInterface(&PageInfo, "js/homeScript.js")
-	f.ExecuteTemplate(w, tmpl, PageInfo)
+	f.MakeTemplateAndExecute(w, r, PageInfo, "templates/home.html")
 }
